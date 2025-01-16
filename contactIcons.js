aab0b62e6fb94b32a2f5dabb2a39ce5091bbf45b
afedyk <afedyk@comcast.net>
2025-01-16 11:58:09 -0500

    new year, new resume

--- contactIcons.js
+++ contactIcons.js
@@ -1,5 +1,5 @@
 function populateContactIcons() {
-    const svgString = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#0A2C5C" class="bi bi-placeholder" viewBox="0 0 16 16" xmlns:xlink="http://www.w3.org/1999/xlink">';
+    const svgString = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#0A2C5C" class="bi bi-placeholder" viewBox="0 0 16 16" xmlns:xlink="http://www.w3.org/1999/xlink">';
     const urlString = '<a xlink:href=placeholder>';
     const pathString = '<path fill="#0A2C5C" d=placeholder />';
     const endString = '</a></svg>';
@@ -9,25 +9,25 @@ function populateContactIcons() {
         .then((text) => {
             // Split the text by lines
             const lines = text.split('\n');
-            // Iterate over each line
-            var tableBody = document.getElementById("iconTable");
-            var newTableBody = document.createElement('tbody');
+
+            // Get the table body element
+            const tableBody = document.getElementById("iconTable");
+            const newTableBody = document.createElement('tbody');
             newTableBody.setAttribute("id", "iconTable");
             tableBody.parentNode.replaceChild(newTableBody, tableBody);
-            let row = newTableBody.insertRow();
-            let cell = 0;
-            lines.forEach((line, index) =>
-            {
-                if (index === 0) return;
-                if (line)
-                {
+
+            lines.forEach((line, index) => {
+                if (index === 0) return; // Skip the header or first line
+                if (line) {
                     const pair = line.split(',');
                     let svgout = svgString.replace("placeholder", pair[0]) +
                         urlString.replace("placeholder", pair[1]) +
                         pathString.replace("placeholder", pair[2]) + endString;
-                    svgout += '<a href='+pair[1]+'>'+' '+pair[0]+'</a>';
-                    //console.log(svgout);
-                    let iconCell = row.insertCell(cell++);
+                    svgout += '<a href=' + pair[1] + '>' + ' ' + pair[0] + '</a>';
+
+                    // Create a new row for each icon
+                    let row = newTableBody.insertRow();
+                    let iconCell = row.insertCell(0);
                     iconCell.innerHTML = svgout;
                 }
             });
